6870e0ccd3521611ad8d5f76f69ce9879ad76444
Alokananda Y <plantsfortrees@gmail.com>
2024-10-19 07:53:18 +0530

    Completed the cake page

--- frontend-careerpath/learn-javascript/challenges/basic_concatenation/script.js
+++
@@ -1,8 +0,0 @@
-const concatStrings = (firstString, secondString) => {
-  return `${firstString} ${secondString}`;
-};
-
-const firstName = "Alokananda";
-const lastName = "Y";
-const fullName = concatStrings(firstName, lastName);
-console.log(fullName);
--- frontend-careerpath/learn-javascript/challenges/template_literals/script.js
+++
@@ -1,9 +0,0 @@
-const formatGreeting = (message, name, greeting) => {
-  return `${message} ${name}! ${greeting}`;
-};
-
-const name = "Alok";
-const message = "Hello";
-const greeting = "Good day!";
-
-console.log(formatGreeting(name, message, greeting));
