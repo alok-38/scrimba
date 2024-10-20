61be9bc406ef2de4c37a8d17f6a46d0d89e64955
Alokananda Y <plantsfortrees@gmail.com>
2024-10-20 09:05:18 +0530

    Create a function that increments the lapsCompleted variable with one

--- frontend-careerpath/JavaScript/functions/lap_times/script.js
+++ frontend-careerpath/JavaScript/functions/lap_times/script.js
@@ -1,5 +1,18 @@
 const sumOfAllLaps = (lap1 = 34, lap2 = 33, lap3 = 36) => {
-	return lap1 + lap2 + lap3;
-}
+  return lap1 + lap2 + lap3;
+};
 
 console.log(sumOfAllLaps());
+
+let lapsCompleted = 0;
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+const incrementLapsCompleted = () => {
+  console.log(lapsCompleted++);
+};
+
+for (let index = 0; index < 3; index++) {
+  incrementLapsCompleted();
+}
