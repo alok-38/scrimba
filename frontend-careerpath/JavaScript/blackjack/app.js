e5a6cf346a660389a09c9eb2249bf26b50d67f76
Alokananda Y <plantsfortrees@gmail.com>
2024-10-28 09:20:47 +0530

    Arrays and objects

--- frontend-careerpath/JavaScript/blackjack/app.js
+++ frontend-careerpath/JavaScript/blackjack/app.js
@@ -4,25 +4,29 @@ let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 
-// Declare a variable called message and assign its value to an empty string
 let message = "";
-
-let messageEl = document.getElementById("message-el");
-// store the sum paragraph in a variable called sumEl
-let sumEl = document.getElementById("sum-el");
-// store the cards paragraph in a varialbe called cardsEl
-let cardsEl = document.querySelector("#cards-el");
-
-// Create a start game function and move the logic inside
+const messageEl = document.getElementById("message-el");
+const sumEl = document.getElementById("sum-el");
+const cardsEl = document.querySelector("#cards-el");
 
 function startGame() {
-  // Render the cards on the page using this format -> "Cards: 10 4"
+  renderGame();
+}
+
+function renderGame() {
+  updateDisplay();
+  setMessage();
+}
+
+function updateDisplay() {
   cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`;
-  // 3. Render the sum on the page using this format -> "Sum: 14"
   sumEl.textContent = `Sum: ${sum}`;
-  // Reassign the message variable to the string we're logging out
+  messageEl.textContent = message;
+}
+
+function setMessage() {
   if (sum <= 20) {
-    message = "Do you want to pick another card? ";
+    message = "Do you want to pick another card?";
   } else if (sum === 21) {
     message = "Wohoo! Blackjack!";
     hasBlackJack = true;
@@ -30,12 +34,11 @@ function startGame() {
     message = "You're out of the game!";
     isAlive = false;
   }
-
-  // Display the message in the messageEl text content
-  messageEl.textContent = message;
 }
 
-// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
 function newCard() {
+  const card = Math.floor(Math.random() * 10) + 2;
+  sum += card;
   messageEl.textContent = "Drawing a new card from the deck!";
+  renderGame();
 }
