f9c87fbf42abe662232db3ffd0df42e7e4215766
Alokananda Y <plantsfortrees@gmail.com>
2024-10-28 08:29:34 +0530

    Drawing a new card

--- frontend-careerpath/JavaScript/blackjack/app.js
+++ frontend-careerpath/JavaScript/blackjack/app.js
@@ -10,10 +10,14 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 // store the sum paragraph in a variable called sumEl
 let sumEl = document.getElementById("sum-el");
+// store the cards paragraph in a varialbe called cardsEl
+let cardsEl = document.querySelector("#cards-el");
 
 // Create a start game function and move the logic inside
 
-const startGame = () => {
+function startGame() {
+  // Render the cards on the page using this format -> "Cards: 10 4"
+  cardsEl.textContent = `Cards: ${firstCard} ${secondCard}`;
   // 3. Render the sum on the page using this format -> "Sum: 14"
   sumEl.textContent = `Sum: ${sum}`;
   // Reassign the message variable to the string we're logging out
@@ -29,7 +33,9 @@ const startGame = () => {
 
   // Display the message in the messageEl text content
   messageEl.textContent = message;
-};
+}
 
-const buttonEl = document.querySelector("button");
-buttonEl.addEventListener("click", startGame);
+// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+function newCard() {
+  messageEl.textContent = "Drawing a new card from the deck!";
+}
