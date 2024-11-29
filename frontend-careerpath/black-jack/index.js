833e05431ebf7a09457606afd93cda6e9886665b
Alokananda Y <plantsfortrees@gmail.com>
2024-11-29 14:29:31 +0000

    Continuing the blackjack game

--- frontend-careerpath/black-jack/index.js
+++ frontend-careerpath/black-jack/index.js
@@ -1,21 +1,27 @@
 let firstCard = 10
-let secondCard = 11
-let sum = firstCard + secondCard + 4
+let secondCard = 4
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-// 1. Declare a variable called message and assign its value to an empty string
-let message = "";
+let message = ""
 
-// 2. Reassign the message variable to the string we're logging out
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack! 🥳")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game! 😭")
-    isAlive = false
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+
+const startGame = () => {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    
+    console.log(message)
 }
 
-// 3. Log it out!
-console.log(hasBlackJack);
+const buttonEl = document.querySelector('button');
+
+buttonEl.addEventListener('click', startGame);
