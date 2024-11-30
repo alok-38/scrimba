aaa183d6481ebd963c7de57b13fdf706f05df9d2
Alokananda Y <plantsfortrees@gmail.com>
2024-11-30 02:25:14 +0000

    Improving the blackjack game

--- frontend-careerpath/black-jack/index.js
+++ frontend-careerpath/black-jack/index.js
@@ -8,20 +8,44 @@ let message = ""
 // 2. Create a startGame() function. Move the conditional
 // below (line 11-20) inside the body of the function.
 
+// Store the message-el paragraph in a variable called messageEl
+const messageEl = document.getElementById('message-el');
+
+// Store the sum in a variable callled sumEl
+let sumEl = document.getElementById('sum-el');
+
+// Store the cards in a variable called cardsEl
+let cardsEl = document.getElementById('cards-el');
+
 const startGame = () => {
+    // Render the sum on the page using this format -> "Sum 14"
+    sumEl.textContent = `Sum: ${sum}`;
+    // Render the cards -> Cards: 10 4
+    cardsEl.textContent = "Cards: " + firstCard + "," + secondCard;
     if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
+        message = "Do you want to draw a new card?";
     } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
+        message = "Wohoo! You've got Blackjack!";
         hasBlackJack = true
     } else {
-        message = "You're out of the game! 😭"
+        message = "You're out of the game!";
         isAlive = false
     }
-    
-    console.log(message)
+
+    // Display the message
+    messageEl.textContent = message;
+
 }
 
-const buttonEl = document.querySelector('button');
+// Create a function newCard() that logs out "Drawing a new card from deck"
+const newCard = () => {
+    console.log("Drawing a new card from deck");
+}
 
-buttonEl.addEventListener('click', startGame);
+
+
+const startButtonEl = document.querySelector('#start-btn');
+const newButtonEl = document.querySelector('#new-btn');
+
+startButtonEl.addEventListener('click', startGame);
+newButtonEl.addEventListener('click', newCard);
