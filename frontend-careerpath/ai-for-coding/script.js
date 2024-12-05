86f5f95e7b4832f946758c90ec084cb5ba078c82
Alokananda Y <plantsfortrees@gmail.com>
2024-12-05 04:07:24 +0000

    AI for coding students

---
+++ frontend-careerpath/ai-for-coding/script.js
@@ -0,0 +1,4 @@
+// Try to modify the expression so that we get a range from 1 to 6
+let randomNumber = Math.floor(Math.random() * 6);
+console.log(randomNumber);
+// Pasting the above code into chatGPT
