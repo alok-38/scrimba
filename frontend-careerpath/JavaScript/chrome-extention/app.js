853624b2df7af7253e979694aaf52ec4578e0ac7
Alokananda Y <plantsfortrees@gmail.com>
2024-12-07 03:32:46 +0000

    Creating a calculator

--- frontend-careerpath/JavaScript/chrome-extention/app.js
+++ frontend-careerpath/JavaScript/chrome-extention/app.js
@@ -1,29 +1,20 @@
-let myLeads = [];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-// Push the value from the inputEl into the myLeads array
-const pushToMyLeads = () => {
-    let lead = inputEl.value;
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
 
-    // Add the lead to the array only if it's not empty
-    if (lead) {
-        myLeads.push(lead);
-
-        // Clear the list before appending new leads
-        ulEl.innerHTML = "";
-
-        // Loop through the array and append the new list item
-        for (let i = 0; i < myLeads.length; i++) {
-            let liEl = document.createElement("li");
-            liEl.textContent = myLeads[i];
-            ulEl.appendChild(liEl);
-        }
-
-        // Clear the input field
-        inputEl.value = "";
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        // Refactor the code to use a template string
+        listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`
     }
-};
+    ulEl.innerHTML = listItems
+}
 
-inputBtn.addEventListener('click', pushToMyLeads);
---
+++ frontend-careerpath/JavaScript/little-calculator/app.js
@@ -0,0 +1,4 @@
+const firstInputEl = document.getElementById("num1");
+const secondInputEl = document.getElementById("num2");
+const btnEl = document.getElementById("btn-add");
+
