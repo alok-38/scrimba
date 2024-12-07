c41522a20918c1b48f539d2df20413fe31f6dee0
Alokananda Y <plantsfortrees@gmail.com>
2024-12-07 05:25:06 +0000

    Deploying to chrome

--- frontend-careerpath/JavaScript/chrome-extention/app.js
+++ frontend-careerpath/JavaScript/chrome-extention/app.js
@@ -1,20 +1,30 @@
-let myLeads = []
+let myLeads = []  // Array to store leads temporarily
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+// Load leads from localStorage when the page loads
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage  // If there are leads in localStorage, restore them
+    renderLeads()  // Render the leads list
+}
+
+// Event listener to add a new lead
 inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
+    if (inputEl.value) {  // Make sure the input is not empty
+        myLeads.push(inputEl.value) // Add the new lead to the array
+        inputEl.value = "" // Clear the input field
+        localStorage.setItem("myLeads", JSON.stringify(myLeads)) // Save the array to localStorage
+        renderLeads() // Re-render the list
+    }
 })
 
+// Function to render the leads list
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        // Refactor the code to use a template string
         listItems += `<li><a target='_blank' href='${myLeads[i]}'>${myLeads[i]}</a></li>`
     }
     ulEl.innerHTML = listItems
 }
-
