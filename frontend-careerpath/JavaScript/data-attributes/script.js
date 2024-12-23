731fa2121b86a7f6b75f45bf6a0cec9ca3e5a5e5
Alokananda Y <plantsfortrees@gmail.com>
2024-12-23 11:01:16 +0000

    Event: target property

--- frontend-careerpath/JavaScript/data-attributes/script.js
+++ frontend-careerpath/JavaScript/data-attributes/script.js
@@ -1,6 +1,7 @@
 const allBtnEl = document.querySelectorAll('button');
+
 allBtnEl.forEach(button => {
-    button.addEventListener('click', () => {
-        console.log(parseInt(button.dataset.clicks));
+    button.addEventListener('click', function (event) {
+        console.log("Button clicked:", event.target);
     })
 })
