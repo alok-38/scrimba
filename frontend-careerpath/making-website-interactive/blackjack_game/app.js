a88321e00a79099a7d72d5d703ebed6e8af126df
Alokananda Y <plantsfortrees@gmail.com>
2025-02-19 02:57:24 +0000

    Getting random card

--- frontend-careerpath/making-website-interactive/blackjack_game/app.js
+++ frontend-careerpath/making-website-interactive/blackjack_game/app.js
@@ -1,22 +1,49 @@
 let firstCard = 10;
-let secondCard = 11;
-let sum = firstCard + secondCard + 4;
+let secondCard = 4;
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
 let hasBlackJack = false;
-let isAlive = true;
-
-// 1. Declare a variable called message and assign its value to an empty string
+let isalive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+const startBtn = document.getElementById("start-btn");
+const newBtn = document.getElementById("new-btn");
 
-// 2. Reassign the message variable based on the game result
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳";
-    hasBlackJack = true;
-} else {
-    message = "You're out of the game! 😭";
-    isAlive = false;
+// Create a function, getRandomCard(), that always returns the number 5
+function getRandomCard() {
+    return 5;
 }
 
-// 3. Log it out!
-console.log(message);
+function startGame() {
+    renderGame();
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: ";
+    for (let index = 0; index < cards.length; index++) {
+        cardsEl.textContent += cards[index] + " ";
+    }
+    sumEl.textContent = "Sum: " + sum;
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isalive = false;
+    }
+    messageEl.textContent = message;
+}
+
+function newCard() {
+    let card = 6;
+    sum += card;
+    cards.push(card);
+    renderGame();
+}
+
+newBtn.addEventListener('click', newCard);
+startBtn.addEventListener('click', startGame);
