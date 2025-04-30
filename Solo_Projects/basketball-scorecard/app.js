43995d72799728911adf32a5dcdeb6f584ede5f3
Alokananda Y <plantsfortrees@gmail.com>
2025-04-30 07:02:38 +0000

    Highlighting the leader

--- Solo_Projects/basketball-scorecard/app.js
+++ Solo_Projects/basketball-scorecard/app.js
@@ -9,17 +9,21 @@ const guestBtnOne = document.getElementById('guest_btn_1');
 const guestBtnTwo = document.getElementById('guest_btn_2');
 const guestBtnThree = document.getElementById('guest_btn_3');
 
+const resetBtn = document.getElementById('reset');
+
 let homeScore = 0;
 let guestScore = 0;
 
 const incrementHome = (points) => {
     homeScore += points;
     homeDisplayEl.textContent = homeScore;
+    highlightTheLeader();
 }
 
 const incrementGuest = (points) => {
     guestScore += points;
     guestDisplayEl.textContent = guestScore;
+    highlightTheLeader();
 }
 
 homeBtnOne.addEventListener('click', () => incrementHome(1));
@@ -29,3 +33,27 @@ homeBtnThree.addEventListener('click', () => incrementHome(3));
 guestBtnOne.addEventListener('click', () => incrementGuest(1));
 guestBtnTwo.addEventListener('click', () => incrementGuest(2));
 guestBtnThree.addEventListener('click', () => incrementGuest(3));
+
+const newGame = () => {
+    homeScore = 0;
+    guestScore = 0;
+    homeDisplayEl.textContent = homeScore;
+    guestDisplayEl.textContent = guestScore;
+    highlightTheLeader();
+}
+
+resetBtn.addEventListener('click', newGame);
+
+// Highlighting the leader
+const highlightTheLeader = () => {
+    if (homeScore > guestScore) {
+        homeDisplayEl.classList.add('leader');
+        guestDisplayEl.classList.remove('leader');
+    } else if (guestScore > homeScore) {
+        guestDisplayEl.classList.add('leader');
+        homeDisplayEl.classList.remove('leader');
+    } else {
+        homeDisplayEl.classList.remove('leader');
+        guestDisplayEl.classList.remove('leader');
+    }
+};
