43dcedc05b10595f29aa1b4c345ab8aa765064fc
plantsfortrees@gmail.com <Alokananda Y>
2025-12-01 08:15:25 +0000

    Creating a unit conversion app

---
+++ The-Fullstack-Developer-Path/JavaScript/practice-time/render_images/index.js
@@ -0,0 +1,19 @@
+// Create a function that renders the three team images
+// Use a for loop, template strings (``), plus equals (+=)
+// .innerHTML to solve the challenge.
+
+const imgs = [
+    "images/hip1.jpg",
+    "images/hip2.jpg",
+    "images/hip3.jpg"
+]
+
+const containerEl = document.getElementById('container');
+
+const renderImages = () => {
+    for (let i = 0; i < imgs.length; i++) {
+        containerEl.innerHTML += `<img class="team-img" src="${imgs[i]}" />`;
+    }
+};
+
+renderImages();
--- The-Fullstack-Developer-Path/projects/unit-conversion/app.js
+++
@@ -1,8 +0,0 @@
-const inputEl = document.querySelector('input');
-const convertButton = document.getElementById('convert');
-const resultEL = document.querySelector('p');
-
-convertButton.addEventListener('click', () => {
-    const inputValue = parseInt(inputEl.value);
-    resultEL.textContent = inputValue;
-});
