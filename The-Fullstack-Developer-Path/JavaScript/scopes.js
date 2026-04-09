2b3d4d325069dbf31c96ba8350989fe4ed1409a9
Alokananda Y <plantsfortrees@gmail.com>
2026-04-09 14:47:34 +0530

    Working with ES6 features

---
+++ The-Fullstack-Developer-Path/JavaScript/scopes.js
@@ -0,0 +1,11 @@
+const message = "Welcome";
+
+function showMessage() {
+    if (true) {
+        let message = "Hello";
+        console.log(message);
+    }
+    console.log(message);
+}
+
+showMessage();
