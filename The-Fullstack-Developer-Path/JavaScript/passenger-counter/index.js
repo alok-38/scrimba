54cc60df423ac06026f66e9110e75459b7fd936b
Alokananda Y <plantsfortrees@gmail.com>
2026-04-15 04:12:13 +0530

    Working with event listeners

---
+++ The-Fullstack-Developer-Path/JavaScript/passenger-counter/index.js
@@ -0,0 +1,20 @@
+// document.getElementById("count").innerText = 5
+
+let count = 0
+
+console.log(count)
+
+
+// 1. Create a variable, myAge, and set its value to your age
+
+// 2. Log the myAge variable to the console
+let myAge = 41;
+console.log(myAge);
+
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+let humanDogRatio;
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);
