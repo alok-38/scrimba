9e735632231b5334a1600cbcb782e6d7700a4e77
Alokananda Y <plantsfortrees@gmail.com>
2026-04-15 16:37:14 +0530

    Deploying the passenger counter app

--- The-Fullstack-Developer-Path/JavaScript/passenger-counter/index.js
+++ The-Fullstack-Developer-Path/JavaScript/passenger-counter/index.js
@@ -1,11 +1,24 @@
-const incrementBtn = document.getElementById('increment-btn');
+const incrementBtn = document.getElementById("increment-btn");
 
-let countEl = document.getElementById('count');
+let countEl = document.getElementById("count-el");
 
 let initialCount = parseInt(countEl.textContent);
 
 const increment = () => {
-    countEl.textContent = ++initialCount;
-}
+  countEl.textContent = ++initialCount;
+};
 
-incrementBtn.addEventListener('click', increment);
+incrementBtn.addEventListener("click", increment);
+
+const saveBtn = document.getElementById("save-btn");
+
+let saveEl = document.getElementById("save-el");
+
+const save = () => {
+  let countAndDash = initialCount + " - ";
+  saveEl.innerText += "" + countAndDash;
+  initialCount = 0;
+  countEl.textContent = initialCount;
+};
+
+saveBtn.addEventListener("click", save);
