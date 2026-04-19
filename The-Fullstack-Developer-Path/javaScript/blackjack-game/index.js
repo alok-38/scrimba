9031cde7696db741c69620a3d78f1740768fe2da
Alokananda Y <plantsfortrees@gmail.com>
2026-04-19 07:22:30 +0530

    Adding functionality

--- The-Fullstack-Developer-Path/javaScript/blackjack-game/index.js
+++ The-Fullstack-Developer-Path/javaScript/blackjack-game/index.js
@@ -1,18 +1,26 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
+let firstCard = 10;
+let secondCard = 4;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card? 🙂"
-} else if (sum === 21) {
-    message = "Wohoo! You've got Blackjack! 🥳"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! 😭"
-    isAlive = false
-}
+const startButtonEl = document.getElementById("start-btn");
 
-console.log(message)
+// 2. Create a startGame() function. Move the conditional
+// below (line 11-20) inside the body of the function.
+
+const startGame = () => {
+  if (sum === 21) {
+    message = "Wohoo! You've got Blackjack! 🥳";
+    hasBlackJack = true;
+  } else if (sum <= 20) {
+    message = "Do you want to draw a new card? 🙂";
+  } else {
+    message = "You're out of the game! 😭";
+    isAlive = false;
+  }
+  console.log(message);
+};
+
+startButtonEl.addEventListener("click", startGame);
