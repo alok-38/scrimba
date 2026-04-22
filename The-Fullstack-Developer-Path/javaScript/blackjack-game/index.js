7c0c00751149cf0ba0e7458909501200bb0b8511
Alokananda Y <plantsfortrees@gmail.com>
2026-04-22 08:53:41 +0530

    Working on the random number generator

--- The-Fullstack-Developer-Path/javaScript/blackjack-game/index.js
+++ The-Fullstack-Developer-Path/javaScript/blackjack-game/index.js
@@ -1,34 +1,42 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let firstCard = Math.random();
+let secondCard = Math.random();
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
+  renderGame();
 }
 
+function renderGame() {
+  // Create a for loop that renders out all the cards instead of just two
+  for (let card = 0; card < cards.length; card++) {
+    cardsEl.textContent = "Cards: " + cards.join(" ");
+  }
+
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+}
 
 function newCard() {
-  messageEl.textContent = "Drawing a new card from the deck!";
-  // Generate a random card value between 2 and 11 (inclusive)
-  let card = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
-  // Add the new card to the sum variable
+  let card = 6;
   sum += card;
-  startGame();
+  // Push the card to the cards array
+  cards.push(card);
+  console.log(cards);
+  renderGame();
 }
