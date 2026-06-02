14851b7c59aae064c75e80d9a31700dfbec393c5
ThePrimeagen <the.primeagen@aol.com>
2026-06-02 10:08:09 +0530

    Working with local storage

---
+++ The-Fullstack-Developer-Path/javaScript/chrome-extention/app.js
@@ -0,0 +1,8 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener('click', () => {
+    console.log("Button clicked");
+})
