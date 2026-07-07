3b524c8ad9dd47665108bc11adaeafaa5175c6eb
ThePrimeagen <the.primeagen@aol.com>
2026-07-07 07:01:17 +0530

    Use createElement() and append() instead of innerHTML

--- The-Fullstack-Developer-Path/javaScript/chrome-extention/script.js
+++ The-Fullstack-Developer-Path/javaScript/chrome-extention/script.js
@@ -1,35 +1,12 @@
-// Create two variables:
-// myLeads -> should be assigned to an empty array
-// inputEl -> should be assigned to the text input field
-let myLeads = [];
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
 const errorEl = document.getElementById("error-el");
-const ulEl = document.getElementById('ul-el');
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
-const saveLead = () => {
-  const inputValue = inputEl.value.trim();
+inputBtn.addEventListener("click", () => {
+  myLeads.push(inputEl.value.trim());
+  console.log(myLeads);
+});
 
-  if (inputValue === "") {
-    errorEl.textContent = "Leads cannot be empty!";
-    errorEl.style.color = "red";
-    return;
-  }
 
-  myLeads.push(inputValue);
-
-  ulEl.innerHTML = ""; // clear list first
-
-  // Log out the items in the myLeads array using a for loop
-  for (let index = 0; index < myLeads.length; index++) {
-    const leads = myLeads[index];
-    const liEl = document.createElement('li');
-    liEl.textContent = leads;
-    ulEl.appendChild(liEl);
-  }
-
-  inputEl.value = "";
-  errorEl.textContent = "";
-};
-
-inputBtn.addEventListener("click", saveLead);
--- The-Fullstack-Developer-Path/javaScript/inner-html/script.js
+++ The-Fullstack-Developer-Path/javaScript/inner-html/script.js
@@ -1,4 +1,17 @@
-// Use .innerHTML to render a Buy! button inside the div container
-
 const containerEl = document.getElementById('container');
+
+// Render button
 containerEl.innerHTML = `<button>Buy</button>`;
+
+// Select button
+const buttonEl = document.querySelector('button');
+
+// Add click event
+buttonEl.addEventListener('click', () => {
+    // Prevent duplicate messages
+    if (!containerEl.querySelector('p')) {
+        const thankyouEl = document.createElement('p');
+        thankyouEl.textContent = "Thank you for buying!";
+        containerEl.appendChild(thankyouEl);
+    }
+});
