7acf5ecadf74763fd98aadcd07c4e31298b94c86
ThePrimeagen <the.primeagen@aol.com>
2026-07-08 09:39:50 +0530

    Rendering leads

--- The-Fullstack-Developer-Path/javaScript/chrome-extention/script.js
+++ The-Fullstack-Developer-Path/javaScript/chrome-extention/script.js
@@ -1,12 +1,15 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
-const inputEl = document.getElementById("input-el");
-const errorEl = document.getElementById("error-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
+const myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+const inputEl = document.getElementById('input-el');
+const saveInputEl = document.getElementById('input-btn');
+const ulEl = document.getElementById('ul-el');
 
-inputBtn.addEventListener("click", () => {
-  myLeads.push(inputEl.value.trim());
-  console.log(myLeads);
-});
+const renderLeads = () => {
+  ulEl.innerHTML = "";
+  myLeads.forEach(lead => {
+    const liEl = document.createElement('li');
+    liEl.textContent = lead;
+    ulEl.appendChild(liEl);
+  });
+}
 
-
+saveInputEl.addEventListener('click', saveLeads);
